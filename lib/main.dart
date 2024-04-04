c117889139e89fe971397d45fcefa87bdef189f0
Obasan Tolulope <obasantolu@gmail.com>
2024-04-04 08:54:08 +0100

    Added the screenUtil Package to responsiveness

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker2/screens/home/views/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,9 +10,12 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Your App',
-      home: SplashScreen(),
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      builder: (_, child) => const MaterialApp(
+        title: 'Your App',
+        home: SplashScreen(),
+      ),
     );
   }
 }
